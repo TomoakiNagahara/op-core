682086fa690ea0a2cbf0c2833ef4890cc9aab395
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2015-06-05 00:35:58 +0900

    New: PDO/ErrorMySQL - Separation of MySQL error.

---
+++ PDO/ErrorMySQL.class.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * ErrorMySQL.class.php
+ * 
+ * @creation  2015-05-08
+ * @version   1.0
+ * @package   op-core/SQL
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2015 (C) Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * ErrorMySQL
+ * 
+ * @creation  2015-05-08
+ * @version   1.0
+ * @package   op-core/SQL
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2015 (C) Tomoaki Nagahara All right reserved.
+ */
+class ErrorMySQL extends OnePiece5
+{
+	private $_driver = 'mysql';
+	
+	function Get($pdo)
+	{
+		//	Get error.
+		$temp = $pdo->errorInfo();
+		$error_id = $temp[0];
+		$error_no = $temp[1];
+	
+		//	Get host and user.
+		$patt = "|'([\.-_a-z0-9]+)'@'([\.-_a-z0-9]+)'|";
+		if( preg_match($patt,$temp[2],$match) ){
+			$user = $match[1];
+			$host = $match[2];
+		}
+	
+		//	Get database and table.
+		$patt_1 = "|'([\.-_a-z0-9]+)'|";
+		$patt_2 = "|'([\.-_a-z0-9]+)\.([\.-_a-z0-9]+)'|";
+		if( preg_match($patt_2,$temp[2],$match) ){
+			//	Escape at backslash.
+			$database = $match[1];
+			$table    = $match[2];
+			$temp[2] = preg_replace($patt, "\\ \\1@\\2 \\", $temp[2]);
+		}else if( preg_match($patt_1,$temp[2],$match) ){
+			//	Escape at backslash.
+			$database = $match[1];
+			$temp[2] = preg_replace($patt, "\\ \\1 \\", $temp[2]);
+		}else{
+			$this->Mark($temp[2]);
+		}
+	
+		//	Branch
+		switch($error_no){
+			case 1044:
+				$message = "This user's access was deny. \\{$user}@{$host}\\";
+				break;
+	
+			case 1062:
+				$message = "Duplicate entry.";
+				break;
+	
+			case 1064:
+				$message = "You have an error in your SQL syntax. ";
+				$message.= "Check the manual that corresponds to your MySQL server version. ";
+				break;
+	
+			case 1091:
+				$message = "Cannot DROP \PRIMARY KEY\. Check that column/key exists.";
+				break;
+	
+			case 1142:
+				$message = " DELETE command was denied.";
+				break;
+	
+			case 1146:
+				//	Table '_database_._table_' doesn't exist
+				$database = ConfigSQL::Quote( $database, $this->_driver );
+				$table    = ConfigSQL::Quote( $table,    $this->_driver );
+				$message  = "Table does not exist. ({$database}.{$table})";
+				break;
+	
+			default:
+				$message = $temp[2];
+				$message = preg_replace("|'([-_a-z0-9\.]+)'|", ' \\ \\1 \\ ', $message);
+				$message = rtrim($message).".";
+		}
+	
+		return "{$message} \SQLSTATE:{$error_id}\ ";
+	}	
+}
--- PDO/PDO5.class.php
+++ PDO/PDO5.class.php
@@ -230,8 +230,14 @@ class PDO5 extends OnePiece5
 	
 	private function _query_error()
 	{
-		if( $this->driver == 'mysql' ){
-			$message = $this->_query_error_mysql();
+		if( $this->driver === 'mysql' ){
+			$path = $this->ConvertPath('op:/PDO/ErrorMySQL.class.php');
+			if( include($path) ){
+				$error = new ErrorMySQL();
+				$message = $error->Get($this->pdo);
+			}else{
+				$this->StackError("Failed to the include of ErrorMySQL.");
+			}
 		}else{
 			$temp = $this->pdo->errorInfo();
 			$error_id = $temp[0];
@@ -240,75 +246,6 @@ class PDO5 extends OnePiece5
 		}
 		
 		$this->StackError($message." \n".$this->qu,'en');
-	}
-	
-	private function _query_error_mysql()
-	{
-		//	Get error.
-		$temp = $this->pdo->errorInfo();
-		$error_id = $temp[0];
-		$error_no = $temp[1];
-		
-		//	Get host and user.
-		$patt = "|'([\.-_a-z0-9]+)'@'([\.-_a-z0-9]+)'|";
-		if( preg_match($patt,$temp[2],$match) ){
-			$user = $match[1];
-			$host = $match[2];
-		}
-		
-		//	Get database and table.
-		$patt_1 = "|'([\.-_a-z0-9]+)'|";
-		$patt_2 = "|'([\.-_a-z0-9]+)\.([\.-_a-z0-9]+)'|";
-		if( preg_match($patt_2,$temp[2],$match) ){
-			//	Escape at backslash.
-			$database = $match[1];
-			$table    = $match[2];
-			$temp[2] = preg_replace($patt, "\\ \\1@\\2 \\", $temp[2]);
-		}else if( preg_match($patt_1,$temp[2],$match) ){
-			//	Escape at backslash.
-			$database = $match[1];
-			$temp[2] = preg_replace($patt, "\\ \\1 \\", $temp[2]);
-		}else{
-			$this->Mark($temp[2]);
-		}
-		
-		//	Branch
-		switch($error_no){
-			case 1044:
-				$this->D($temp);
-				$message = "This user's access was deny. \\{$user}@{$host}\\";
-				break;
-				
-			case 1062:
-				$this->D($temp);
-				$message = "Duplicate entry.";
-				break;
-				
-			case 1064:
-				$this->D($temp);
-				$message = "You have an error in your SQL syntax. "; 
-				$message.= "Check the manual that corresponds to your MySQL server version. ";
-				break;
-				
-			case 1091:
-				$this->D($temp);
-				$message = "Cannot DROP \PRIMARY KEY\. Check that column/key exists.";
-				break;
-				
-			case 1146:
-				//	Table '_database_._table_' doesn't exist
-				$database = ConfigSQL::Quote( $database, $this->driver );
-				$table    = ConfigSQL::Quote( $table,    $this->driver );
-				$message  = "Table does not exist. ({$database}.{$table})";
-				break;
-				
-			default:
-				$message = $temp[2];
-				$message = preg_replace("|'([-_a-z0-9\.]+)'|", ' \\ \\1 \\ ', $message);
-				$message = rtrim($message).".";
-		}
-		
-		return "{$message} \#{$error_no}\ ";
 	}
 	
 	function ConvertCharset( $charset=null )
