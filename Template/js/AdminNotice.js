3fc805aa2d00c512bbc77a1d1f94e4d9d4931e0c
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-01-19 13:53:22 +0900

    New: Notice - WIP, Display to Object and Array.

--- Template/js/AdminNotice.js
+++ Template/js/AdminNotice.js
@@ -10,43 +10,44 @@
 
 	//	Each errors.
 	jQuery.each(__notice__.errors, function(i, error){
-		_generate_each_error(i+1, error);
+		_add_each_error(i+1, error);
 	});
 
-	function _generate_each_error(no, error){
-		_generate_headline(no, error);
+	function _add_each_error(no, error){
+		_add_headline(no, error);
 
 		//	backtrace
 		$table = $('<table/>');
 		$table.addClass('backtrace');
 		$container.append($table);
-		jQuery.each(error.backtrace, function(i, backtrace){
-			_generate_each_backtrace(i+1, backtrace);
+		jQuery.each(error.backtrace.reverse(), function(i, backtrace){
+			_add_each_backtrace(i+1, backtrace);
 		});
 	}
 	
-	function _generate_headline(no, error){
+	function _add_headline(no, error){
 		$headline = $('<div/>');
 		$headline.addClass('headline');
 		$headline.text('#' + no + ' ' + error.message);
 		$container.append($headline);
 	}
 
-	function _generate_each_backtrace(no, backtrace){
-		_generate_backtrace(no, backtrace);
+	function _add_each_backtrace(no, backtrace){
+		_add_backtrace(no, backtrace);
 	}
 
-	function _generate_backtrace(no, backtrace){
-		console.dir(backtrace);
+	function _add_backtrace(no, backtrace){
+	//	console.dir(backtrace);
+
+		var $tr = $('<tr/>');
+		$table.append($tr);
+
 		var file = backtrace.file;
 		var line = backtrace.line;
 		var func = backtrace.function;
 		var clas = backtrace.class ? backtrace.class: '';
 		var type = backtrace.type  ? backtrace.type:  '';
-		var args = _generate_args(backtrace);
-
-		var $tr = $('<tr/>');
-		$table.append($tr);
+		var args = _add_args(backtrace);
 
 		var $td = $('<td/>');
 		$td.text(no);
@@ -69,7 +70,7 @@
 		$tr.append($td);
 	}
 
-	function _generate_args(backtrace){
+	function _add_args(backtrace){
 		var args = '';
 		jQuery.each(backtrace.args, function(i, v){
 			var type = typeof v;
@@ -91,7 +92,12 @@
 					v = '<span class="string">\''+v+'\'</span>';
 					break;
 				case 'object':
-					v = '<span class="object">object</span>';
+					_add_object(v);
+					if( v.hasOwnProperty(0) ){
+						v = '<span class="array">array...</span>';
+					}else{
+						v = '<span class="object">object...</span>';
+					}
 					break;
 				defualt:
 					alert(type);
@@ -101,5 +107,45 @@
 		args = args.replace(/^ /,'');
 		args = args.replace(/,$/,'');
 		return '('+args+')';
+	}
+
+	function _add_object(object){
+		var $tr = $("<tr/>");
+		var $td = $("<td/>");
+
+		//	TR
+		$tr.addClass('object');
+
+		//	TD
+		$td.attr('colspan',4);
+		$td.addClass('object');
+
+		//	append
+		$tr.append($td);
+		$table.append($tr);
+		$td.append( _get_args_table(object) );
+	}
+
+	function _get_args_table(args){
+		var $table = $('<table/>');
+		$table.addClass('object');
+
+		jQuery.each(args, function(i, v){
+			var $tr = $('<tr/>');
+			var $th = $('<th/>');
+			var $td = $('<td/>');
+
+			$table.append($tr);
+			$tr.append($th);
+			$tr.append($td);
+
+			$th.addClass('args');
+			$td.addClass('args');
+
+			$th.html(i);
+			$td.html(v);
+		});
+
+		return $table;
 	}
 })();
