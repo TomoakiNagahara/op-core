01424aa7a7f35d7019e23eb237eb6b09db6f6891
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2016-02-09 23:54:20 +0900

    New: Model/Date

---
+++ Model/Date.model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * op-core/model/Date.model.php
+ * 
+ * Control of world date.
+ * 
+ * @creation  2016-02-07
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Model_Date
+ * 
+ * @creation  2016-02-07
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Model_Date extends Model_Model
+{
+	
+}
